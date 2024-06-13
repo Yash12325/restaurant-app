5d0e779b83e84c641c6f3d349bc2eb66df0f09eb
Yash12325 <yashg12325@gmail.com>
2024-06-13 17:00:49 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Yash</div>
+          <div className="right">
+            <p>Z6 Gulshan-e-Maymar, Jaipur</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By YASH</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By YASH.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
